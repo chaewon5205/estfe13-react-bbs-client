2f98aba5330ae0e73cc75b7271a74c41dde277b6
chaewon5205 <parkjihae9262@gmail.com>
2026-07-02 17:37:10 +0900

    feat: 오늘 배운 것 총 정리

--- src/components/BoardList.jsx
+++ src/components/BoardList.jsx
@@ -1,8 +1,41 @@
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function Board({ data }) {
+  return (
+    <tr>
+      <td>
+        <Form.Check />
+      </td>
+      <td>{data.id}</td>
+      <td>{data.title}</td>
+      <td>{data.writer}</td>
+      <td>{data.date}</td>
+    </tr>
+  );
+}
 
 export default function BoardList() {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/list", {})
+      .then(response => {
+        console.log(response.data);
+        setList(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      })
+      .finally(() => {
+        console.log("요청완료");
+      });
+  }, []);
+
   return (
     <>
       <Table striped bordered hover>
@@ -16,24 +49,9 @@ export default function BoardList() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <Form.Check disabled />
-            </td>
-            <td>1</td>
-            <td>안녕</td>
-            <td>홍길동</td>
-            <td>2026.07.02</td>
-          </tr>
-          <tr>
-            <td>
-              <Form.Check disabled />
-            </td>
-            <td>1</td>
-            <td>안녕</td>
-            <td>홍길동</td>
-            <td>2026.07.02</td>
-          </tr>
+          {list.map(item => (
+            <Board data={item} />
+          ))}
         </tbody>
       </Table>
       <div className="d-flex gap-1 justify-content-end">
